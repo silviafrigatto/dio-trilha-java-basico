78d2ee52ddef5d475e6862bed9228d914edb770a
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-07-31 11:43:01 -0300

    testando exemplo de uso de métodos

---
+++ metodos/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario{
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV ligada: " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(10);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+    }
+}
